b32311c90bacac931950140e47cdb9f49dd179bc
Gensirovsky <gensirovsky.218991@gmail.com>
2022-01-11 18:46:31 +0200

    fix: added nuvHeader subtitle

--- .developing/src/plugins/ecommerce/admin/src/components/Navigation/index.js
+++ .developing/src/plugins/ecommerce/admin/src/components/Navigation/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from "styled-components";
 
 import { LocalMall } from '@material-ui/icons';
 import { Dashboard } from '@material-ui/icons';
@@ -17,6 +18,54 @@ import {
   SubNavSections,
 } from '@strapi/design-system/SubNav';
 
+const SubNavHeaderStyled = styled.div`
+  padding: 24px 16px 8px 24px;
+  div:first-child {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+    h2 {
+      display: block;
+      position: relative;
+      color: #32324d;
+      font-weight: 600;
+      font-size: 1.125rem;
+      line-height: 1.22;
+    }
+    h2:first-child::after {
+      content: "Alpha";
+      position: absolute;
+      font-size: 0.85rem;
+      background-color: #EDBB99;
+      top: -15.5px;
+      right: -40px;
+      padding: 2px 7.5px;
+      border-radius: 10px;
+      color: #DC7633;
+      font-weight: 500;
+    }
+  }
+  
+  div:nth-child(2) {
+    padding-top: 16px;
+    hr {
+      display: block;
+      unicode-bidi: isolate;
+      margin-block-start: 0.5em;
+      margin-block-end: 0.5em;
+      margin-inline-start: auto;
+      margin-inline-end: auto;
+      overflow: hidden;
+      width: 1.5rem;
+      background-color: #dcdce4;
+      height: 1.5px;
+      border: none;
+      margin: 0;
+    }
+  }
+`
+
 
 const Navigation = () => {
   const links = [
@@ -29,7 +78,15 @@ const Navigation = () => {
 
   return (
     <SubNav ariaLabel='Ecommerce'>
-      <SubNavHeader label='Ecommerce' />
+      {/*<SubNavHeader label='Ecommerce' />*/}
+      <SubNavHeaderStyled>
+        <div>
+          <h2>Ecommerce</h2>
+        </div>
+        <div>
+          <hr/>
+        </div>
+      </SubNavHeaderStyled>
       <SubNavSections style={{ marginLeft: '-8px' }}>
           {
             links.map(link =>
--- .developing/src/plugins/ecommerce/admin/src/pages/Orders/RowTable.js
+++ .developing/src/plugins/ecommerce/admin/src/pages/Orders/RowTable.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled from "styled-components"
 
 import { Td } from "@strapi/design-system/Table";
 import { Typography } from "@strapi/design-system/Typography";
