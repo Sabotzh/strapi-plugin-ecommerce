d7e0b70c95c4a960b8b5930c1bada4d1f298eacb
Vladislav Gensirovsky <gensirovsky.218991@gmail.com>
2022-01-13 17:13:18 +0200

    fix: restored server

---
+++ .developing/src/plugins/ecommerce/server/routes/baseRoutes/products.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    method: 'POST',
+    path: '/products',
+    handler: 'products.create',
+    config: {},
+  },
+  {
+    method: 'GET',
+    path: '/products',
+    handler: 'products.find',
+    config: {},
+  },
+  {
+    method: 'GET',
+    path: '/products/:id',
+    handler: 'products.findOne',
+    config: {},
+  },
+  {
+    method: 'GET',
+    path: '/products/by-slug/:slug',
+    handler: 'products.findOneBySlug',
+    config: {},
+  },
+  {
+    method: 'DELETE',
+    path: '/products/:id',
+    handler: 'products.remove',
+    config: {},
+  },
+  {
+    method: 'PUT',
+    path: '/products/:id/publish',
+    handler: 'products.publish',
+    config: {},
+  },
+  {
+    method: 'PUT',
+    path: '/products/:id/un-publish',
+    handler: 'products.unPublish',
+    config: {},
+  },
+];
---
+++ plugin/server/onBootstrap.js
@@ -0,0 +1,4 @@
+module.exports = ({ strapi }) => {
+  console.log('Ecommerce bootstrap');
+  console.log(strapi.plugins.ecommerce.routes)
+};
