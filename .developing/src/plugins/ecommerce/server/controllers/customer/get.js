70f474fcf78f482572b57ee132661a63690ae3d1
Yegor Shtonda <shtyegor@gmail.com>
2022-01-28 21:57:16 +0200

    feat: Added part of customer get controller

--- .developing/src/plugins/ecommerce/server/controllers/customer/get.js
+++ .developing/src/plugins/ecommerce/server/controllers/customer/get.js
@@ -1,3 +1,33 @@
-module.exports = ({ strapi }) => (ctx) => {
-    ctx.body = { message: 'customer get' };
+const jwt = require('jsonwebtoken');
+
+module.exports = ({ strapi }) => async (ctx) => {
+  const isGenerateNewCustomer = true;
+  let customer = null;
+
+  if (isGenerateNewCustomer) {
+    customer = await strapi
+      .query('plugin::ecommerce.customer')
+      .create({
+        data: {
+          firstname: '',
+          middlename: '',
+          lastname: '',
+          isShadow: true,
+          email: '',
+          phone: '',
+          cart: [],
+          wishlist: [],
+        }
+      });
+  }
+
+  const token = jwt.sign(
+    { version: 1, ecommerceCustomerId: customer.id },
+    process.env.JWT_SECRET,
+  );
+
+  ctx.body = {
+    ecommerceJwt: token,
+    data: customer,
+  };
 };
