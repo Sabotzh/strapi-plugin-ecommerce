ae008755295aa3dbb925a38c65a92340977f4433
Yegor Shtonda <shtyegor@gmail.com>
2022-01-28 17:40:43 +0200

    feat: Added postgres db provider & aws-s3 uploader provider & bump strapi to 4.0.6

--- .developing/config/plugins.js
+++ .developing/config/plugins.js
@@ -1,6 +1,19 @@
-module.exports = {
-  'ecommerce': {
-    enabled: true,
-    resolve: './src/plugins/ecommerce'
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+    },
   },
-}
+  ecommerce: {
+    enabled: true,
+    resolve: './src/plugins/ecommerce',
+  },
+});
