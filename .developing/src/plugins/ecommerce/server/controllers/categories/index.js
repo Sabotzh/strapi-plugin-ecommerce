58360a004e0f4c56a21b5dfb6dff2b0df1e1d422
Vladislav Gensirovsky <gensirovsky.218991@gmail.com>
2022-01-31 01:32:41 +0200

    fix: fixed filter categories

--- .developing/src/plugins/ecommerce/server/controllers/categories/index.js
+++ .developing/src/plugins/ecommerce/server/controllers/categories/index.js
@@ -4,6 +4,7 @@ const update = require('./update');
 const find = require('./find');
 const findOne = require('./findOne');
 const findOneBySlug = require('./findOneBySlug');
+const findSorted = require('./findSorted')
 const publish = require('./publish');
 const unPublish = require('./unPublish');
 
@@ -11,6 +12,7 @@ module.exports = (globalCtx) => ({
   create: create(globalCtx),
   remove: remove(globalCtx),
   update: update(globalCtx),
+  findSorted: findSorted(globalCtx),
   find: find(globalCtx),
   findOne: findOne(globalCtx),
   findOneBySlug: findOneBySlug(globalCtx),
