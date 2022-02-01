4e3e4dfd59f643ff9254b8cda9e84f053137cb8d
Yegor Shtonda <shtyegor@gmail.com>
2022-02-01 06:36:49 +0200

    feat: Added customer update controller

--- .developing/src/plugins/ecommerce/server/controllers/customer/update.js
+++ .developing/src/plugins/ecommerce/server/controllers/customer/update.js
@@ -1,3 +1,21 @@
-module.exports = ({ strapi }) => (ctx) => {
-  ctx.body = { message: 'customer update' };
+const strapiForbiddenFields = require('../_utils/strapiForbiddenFieldsForUpdate');
+const getCustomerIdFromctx = require('../_utils/getCustomerIdFromCtx');
+
+module.exports = ({ strapi }) => async (ctx) => {
+  const data = ctx.request.body;
+  const customerId = getCustomerIdFromctx(ctx);
+  if (!customerId) {
+    ctx.status = 403;
+    ctx.body = 'Invalid Ecommerce Authorization';
+    return;
+  }
+
+  const forbiddenFields = [ ...strapiForbiddenFields, 'is_shadow', 'isShadow', 'attached_to_user', 'attachedToUser' ];
+  for (const forbiddenField of forbiddenFields) {
+    delete data[forbiddenField];
+  }
+
+  ctx.body = await strapi
+    .query('plugin::ecommerce.customer')
+    .update({ where: { id: customerId }, data });
 };
