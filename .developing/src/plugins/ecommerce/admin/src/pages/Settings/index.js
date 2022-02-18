6b104f08d48c0ad6f99e0e6263ff475f33a9d99b
Vladislav Gensirovsky <gensirovsky.218991@gmail.com>
2022-02-18 14:38:55 +0200

    feat: settings page

--- .developing/src/plugins/ecommerce/admin/src/pages/Settings/index.js
+++ .developing/src/plugins/ecommerce/admin/src/pages/Settings/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useIntl } from 'react-intl';
-
 import { useFocusWhenNavigate } from '@strapi/helper-plugin';
-import { HeaderLayout } from '@strapi/design-system/Layout';
+import { HeaderLayout, Layout, ContentLayout } from '@strapi/design-system/Layout';
+import { Box } from '@strapi/design-system/Box';
+import { Stack } from '@strap/design-system/Stack';
+import { Typography } from '@strapi/design-system/Typography';
+import { TextInput } from '@strapi/design-system/TextInput';
+import { Grid, GridItem } from '@strapi/design-system/Grid';
+import { Button } from '@strapi/design-system/Button';
+import Key from '@strapi/icons/Key';
 
 import getTrad from '../../utils/getTrad';
 
 const SettingsPage = () => {
+  const [ token, setToken] = useState('');
+
   useFocusWhenNavigate();
+
+  const submit = () => {
+    if (!token) return
+    console.log(token)
+  }
 
   const { formatMessage } = useIntl();
   const title = formatMessage({
@@ -17,7 +30,7 @@ const SettingsPage = () => {
   });
 
   return (
-    <>
+    <Layout>
       <main>
         <HeaderLayout
           title={title}
@@ -26,8 +39,41 @@ const SettingsPage = () => {
             defaultMessage: 'Configure the ecommerce plugin',
           })}
         />
+        <ContentLayout>
+          <Box
+            hasRadius
+            background='neutral0'
+            shadow='tableShadow'
+            paddingTop={7}
+            paddingBottom={7}
+            paddingRight={6}
+            paddingLeft={6}
+          >
+            <Stack size={4}>
+              <Typography variant="delta" as='h2'>Configuration</Typography>
+              <Grid gap={5}>
+                <GridItem col={6} s={12}>
+                  <TextInput
+                    name='token'
+                    label='Token'
+                    onChange={ e => setToken(e.target.value) }
+                    value={ token }
+                  />
+                </GridItem>
+              </Grid>
+              <GridItem col={7} s={12}>
+                <Button
+                  onClick={ submit }
+                  startIcon={<Key />}
+                >
+                  Save
+                </Button>
+              </GridItem>
+            </Stack>
+          </Box>
+        </ContentLayout>
       </main>
-    </>
+    </Layout>
   );
 };
 
